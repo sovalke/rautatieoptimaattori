98dc6fcbd1b1a857d1d2dd44d185ac55167e2829
sovalke <sonja.valkeinen@gmail.com>
2019-08-01 14:26:38 +0300

    A* karvalakkimalli toteutettu

--- src/rautatieoptimaattori/Astar.java
+++ src/rautatieoptimaattori/Astar.java
@@ -6,6 +6,7 @@ import java.util.PriorityQueue;
 public class Astar {
 
     HashMap<String, Solmu> solmut = new HashMap<>();
+    HashMap<Integer, Solmu> solmutId = new HashMap<>();
 
     // Lisätään asema koordinaatteineen.
     void lisaaAsema(String nimi, int id, double x, double y) {
@@ -13,26 +14,47 @@ public class Astar {
             Solmu paivitettava = solmut.get(nimi);
             paivitettava.setXY(x, y);
             paivitettava.setId(id);
+            solmutId.put(id, paivitettava);
         } else {
-            solmut.put(nimi, new Solmu(nimi, id, x, y));
+            Solmu lisattava = new Solmu(nimi, id, x, y);
+            solmut.put(lisattava.getNimi(), lisattava);
+            solmutId.put(lisattava.getId(), lisattava);
         }
         Solmu a = solmut.get(nimi);
         System.out.println("Lisätty asema " + a.getNimi() + " (" + a.getId() + "), koordinaatit " + a.getX() + " / " + a.getY());
     }
 
-    // Lisätään kaari kahden aseman välille.
+    // Lähtöpaikka ja määränpää annetaan merkkijonona.
     void lisaaYhteys(String lahtopaikka, String maaranpaa, int i) {
-        // Keskeytetään, jos asemat puuttuvat.
         if (!solmut.containsKey(lahtopaikka)) {
-            System.out.println("Asema " + lahtopaikka + " puuttuu tietokannasta");
+            System.out.println("Asemaa " + lahtopaikka + " ei löydy");
         }
         if (!solmut.containsKey(maaranpaa)) {
-            System.out.println("Asema " + maaranpaa + " puuttuu tietokannasta");
+            System.out.println("Asemaa " + maaranpaa + " ei löydy");
         }
 
         // Lisätään kaari molempiin suuntiin.
         Solmu a = solmut.get(lahtopaikka);
         Solmu b = solmut.get(maaranpaa);
+        lisaaYhteys(a, b, i);
+    }
+
+    // Lähtöpaikka ja määränpää annetaan id-numerona.
+    void lisaaYhteys(int lahtopaikkaId, int maaranpaaId, int i) {
+        if (!solmutId.containsKey(lahtopaikkaId)) {
+            System.out.println("Asemaa " + lahtopaikkaId + " ei löydy");
+        }
+        if (!solmutId.containsKey(maaranpaaId)) {
+            System.out.println("Asemaa " + maaranpaaId + " ei löydy");
+        }
+
+        // Lisätään kaari molempiin suuntiin.
+        Solmu a = solmutId.get(lahtopaikkaId);
+        Solmu b = solmutId.get(maaranpaaId);
+        lisaaYhteys(a, b, i);
+    }
+
+    void lisaaYhteys(Solmu a, Solmu b, int i) {
         a.lisaaYhteys(b, i);
         b.lisaaYhteys(a, i);
     }
@@ -81,21 +103,23 @@ public class Astar {
         }
     }
 
-    // Varsinainen Dijkstran algoritmi
+    // Varsinainen algoritmi
     void reitinPituus(String lahtopaikka, String maaranpaa) {
 
         // Asetetaan halutut alku- ja päätepisteet.
         Solmu alku = solmut.get(lahtopaikka);
         Solmu loppu = solmut.get(maaranpaa);
 
+        System.out.println("Etäisyysarvio matkalle " + alku.getNimi() + "-" + loppu.getNimi() + " on " + etaisyysArvio(alku, loppu));
+
         // Luodaan keko ja tarvittavat HashMapit.
-        HashMap<Solmu, Integer> etaisyydet = new HashMap<>();
+        HashMap<Solmu, Double> etaisyydet = new HashMap<>();
         HashMap<Solmu, Boolean> kasitelty = new HashMap<>();
         PriorityQueue<VertailtavaSolmu> keko = new PriorityQueue();
 
         // Lisätään lähtöpiste kekoon ja etäisyysarvioon.
         keko.add(new VertailtavaSolmu(alku, 0));
-        etaisyydet.put(alku, 0);
+        etaisyydet.put(alku, 0.0);
 
         while (!keko.isEmpty()) {
 
@@ -107,37 +131,66 @@ public class Astar {
                 continue;
             }
 
+            double pieninArvio = Double.POSITIVE_INFINITY;
+            Solmu valittu = kasiteltava;
+
             // Muutoin käydään läpi solmun naapurit.
             // Miksi tässä ei voi käyttää getNaapurit-metodia? Tulee virheilmoitus.
             for (HashMap.Entry<Solmu, Integer> naapuri : kasiteltava.naapurit.entrySet()) {
 
                 // Onko naapurisolmu jo etäisyysarviotaulukossa?
                 Solmu tutkittavaNaapuri = naapuri.getKey();
-                int uusi = etaisyydet.get(kasiteltava) + kasiteltava.getEtaisyys(tutkittavaNaapuri);
+                double uusi = etaisyydet.get(kasiteltava) + kasiteltava.getEtaisyys(tutkittavaNaapuri);
+                double kokonaisArvio = uusi + etaisyysArvio(tutkittavaNaapuri, loppu);
+                System.out.println("-- Naapuri " + tutkittavaNaapuri.getNimi() + ", f: " + kokonaisArvio);
 
-                // Ei ole; lisää se etäisyysarviointitaulukkoon ja kekoon.
+                // Päivitetään pienin etäisyysarvio ja tallennetaan tieto parhaasta solmusta.
+                if (kokonaisArvio < pieninArvio) {
+                    pieninArvio = kokonaisArvio;
+                    valittu = tutkittavaNaapuri;
+                }
+
+                // Solmu ei ole vielä etäisyystaulukossa; lisätään se
                 if (!etaisyydet.containsKey(tutkittavaNaapuri)) {
                     etaisyydet.put(tutkittavaNaapuri, uusi);
-                    keko.add(new VertailtavaSolmu(tutkittavaNaapuri, uusi));
-
                     continue;
                 }
 
-                // On; verrataan uutta ja vanhaa etäisyyttä
-                int vanha = etaisyydet.get(tutkittavaNaapuri);
+                // Solmu on taulukossa; verrataan uutta ja vanhaa etäisyyttä
+                double vanha = etaisyydet.get(tutkittavaNaapuri);
                 if (uusi < vanha) {
                     etaisyydet.put(tutkittavaNaapuri, uusi);
-
-                    // Siirrytään tutkimaan naapuria.
-                    keko.add(new VertailtavaSolmu(tutkittavaNaapuri, uusi));
                 }
 
+                System.out.println("Määränpäänä " + loppu.getNimi());
+                System.out.println("Valittuna " + valittu.getNimi());
+
             }
+            
+            // Määränpää saavutettu, nyt voi stopata.
+            if (loppu.getId() == valittu.getId()) {
+                break;
+            }
+            System.out.println("Valittu solmuksi " + valittu.getNimi() + ", f: " + pieninArvio);
+            keko.add(new VertailtavaSolmu(valittu, pieninArvio));
 
             kasitelty.put(kasiteltava, true);
 
         }
 
         System.out.println("Etäisyys " + alku.getNimi() + "-" + loppu.getNimi() + " on " + etaisyydet.get(loppu) + " km.");
+        System.out.println("--------------");
+    }
+
+    double etaisyysArvio(Solmu piste, Solmu maaranpaa) {
+        // Hyödynnetään Pythagoraan lausetta etäisyysarvion laskemiseen.
+
+        // Varmistetaan, että etäisyys ilmaistaan itseisarvoina.        
+        double x = Math.abs(maaranpaa.getX() - piste.getX());
+        double y = Math.abs(maaranpaa.getY() - piste.getY());
+
+        // etaisyysaArvio = neliöjuuri(x^2 + y^2)
+        double etaisyysArvio = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) * 100;
+        return etaisyysArvio;
     }
 }
--- src/rautatieoptimaattori/Rautatieoptimaattori.java
+++ src/rautatieoptimaattori/Rautatieoptimaattori.java
@@ -23,19 +23,39 @@ public class Rautatieoptimaattori {
         r.lisaaYhteys("Kuopio","Joensuu",169);
         r.lisaaYhteys("Kuopio","Jyväskylä",164);
         
+        // System.out.println("KÄYTÖSSÄ OLEVAT RADANPÄTKÄT");
+        //r.tulostaReitit();
+        System.out.println("-------------------------------------------");
 
-        System.out.println("KÄYTÖSSÄ OLEVAT RADANPÄTKÄT");
-        r.tulostaReitit();
-        System.out.println("-----------------------");
-
+        System.out.println("Dijkstra vauhdissa...");
         r.reitinPituus("Vaasa", "Helsinki");
         r.reitinPituus("Turku", "Kuopio");
         r.reitinPituus("Turku", "Helsinki");
+        r.reitinPituus("Oulu", "Helsinki");
+        r.reitinPituus("Oulu", "Tampere");
         
+        System.out.println("-------------------------------------------");
+        System.out.println("A* vauhdissa...");
         Astar a = new Astar();
         a.lisaaAsema("Helsinki", 123, 60.166640, 24.943536);
         a.lisaaAsema("Turku", 124, 60.451389, 22.266667);
         a.lisaaAsema("Tampere", 125, 61.498056, 23.760833);
+        a.lisaaAsema("Oulu", 126, 65.013785, 25.472099);
+        a.lisaaAsema("Seinäjoki", 127, 62.790278, 22.840278);
+        
+        a.lisaaYhteys("Helsinki", "Tampere", 187);
+        a.lisaaYhteys("Helsinki","Turku",193);
+        a.lisaaYhteys(124,125,170);
+        a.lisaaYhteys("Seinäjoki","Oulu",335);
+        a.lisaaYhteys("Tampere","Seinäjoki",160);
+        a.tulostaReitit();
+        
+        a.reitinPituus("Turku", "Tampere");
+        a.reitinPituus("Oulu", "Helsinki");
+        a.reitinPituus("Oulu", "Tampere");
+        
+        
+        
     }
 
 }
