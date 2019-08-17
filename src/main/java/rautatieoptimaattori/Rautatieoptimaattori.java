b558743679bda18d633196e539c66c5280d43868
sovalke <sonja.valkeinen@gmail.com>
2019-08-17 12:16:19 +0300

    Päivitetty riippuvuuksia

--- src/main/java/rautatieoptimaattori/Rautatieoptimaattori.java
+++ src/main/java/rautatieoptimaattori/Rautatieoptimaattori.java
@@ -10,23 +10,22 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import rautatieoptimaattori.domain.Verkko;
 import rautatieoptimaattori.io.Aineistokasittelija;
 import tietorakenteet.OmaLista;
-import tietorakenteet.OmaPari;
 
 public class Rautatieoptimaattori {
 
     public static void main(String[] args) {
 
         try {
-//            Aineistokasittelija data = new Aineistokasittelija();
-//            int asemaLkm = data.lisaaAsemat("./data/stations.csv");
-//            System.out.println("Asemia on nyt " + asemaLkm + ".");
+            Aineistokasittelija data = new Aineistokasittelija();
+            int asemaLkm = data.lisaaAsemat("./data/stations.csv");
+            System.out.println("Asemia on nyt " + asemaLkm + ".");
+            
+            data.lisaaYhteydet("./data/trains.csv");
+            Verkko verkko = data.getVerkko();
 //            
-//            data.lisaaYhteydet("./data/trains.csv");
-//            Verkko verkko = data.getVerkko();
-//            
-//            verkko.tulostaReitit();
 //            
 //            Astar r = new Astar(verkko);
 //            r.reitinPituus(verkko.getSolmu(130), verkko.getSolmu(1));
@@ -34,18 +33,20 @@ public class Rautatieoptimaattori {
 //            Dijkstra d = new Dijkstra(verkko);
 //            d.reitinPituus(130, 1);
 
-            OmaLista<String> lista = new OmaLista<>();
-            System.out.println(lista.contains("hei"));
-            lista.add("hei");
-            System.out.println(lista.contains("hei"));
-            int indeksi = lista.indexOf("hei");
-            System.out.println(indeksi);
-            System.out.println(lista.value(indeksi));
-            lista.remove("hei");
-            System.out.println(lista.contains("hei"));
+//            OmaLista<String> lista = new OmaLista<>();
+//            System.out.println(lista.contains("hei"));
+//            lista.add("hei");
+//            System.out.println(lista.contains("hei"));
+//            int indeksi = lista.indexOf("hei");
+//            System.out.println(indeksi);
+//            System.out.println(lista.value(indeksi));
+//            lista.remove("hei");
+//            System.out.println(lista.contains("hei"));
+//
+//            OmaPari<String, Integer> pari = new OmaPari<>("yksi", 1);
+//            System.out.println(pari.getKey() + " -> " + pari.getValue());
 
-            OmaPari<String, Integer> pari = new OmaPari<>("yksi", 1);
-            System.out.println(pari.getKey() + " -> " + pari.getValue());
+              OmaLista<String> lista = verkko.Reitit();
 
         } catch (Exception ex) {
             System.out.println("VIRHE: " + ex.getMessage());
